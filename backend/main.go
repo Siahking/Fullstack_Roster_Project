f27c46252de3850ff30835f30ae704cf00b3bcde
josiahking <josiahk2016@gmail.com>
2026-05-12 13:40:35 -0400

    edited cors to use vercel

--- backend/main.go
+++ backend/main.go
@@ -36,7 +36,7 @@ func main() {
 
 	//Allow cors
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://127.0.0.1:5501", "http://127.0.0.1:5500", "http://localhost:5500"},
+		AllowOrigins:     []string{"http://127.0.0.1:5501", "http://127.0.0.1:5500", "http://localhost:5500","https://fullstack-roster-project.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
